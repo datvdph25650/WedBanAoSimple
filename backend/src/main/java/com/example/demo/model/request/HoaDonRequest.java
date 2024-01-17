86248051d11fce3f4eac01524ba2fc4a327606b6
unknown <cubervd74@gmail.com>
2024-01-17 20:50:19 +0700

    duan

---
+++ backend/src/main/java/com/example/demo/model/request/HoaDonRequest.java
@@ -0,0 +1,28 @@
+package com.example.demo.model.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class HoaDonRequest {
+
+    private String trangThai;
+
+    private String tenKhachHang;
+
+    private String diaChiKhachHang;
+
+    private String soDienThoaiKhachHang;
+
+    private Double phiVanChuyen;
+
+    private UUID idKhachHang;
+
+    private Timestamp ngayThanhToan;
+
+    private Double tongTien;
+}
